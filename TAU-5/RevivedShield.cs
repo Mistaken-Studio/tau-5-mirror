582589d03f1fec8b01047ef60a155b3728721949
Wydział Wewnętrzny <g_sw@poczta.fm>
2021-11-20 16:18:24 +0100

    RevivedShield fix

--- TAU-5/RevivedShield.cs
+++ TAU-5/RevivedShield.cs
@@ -1,27 +1,21 @@
 ﻿// -----------------------------------------------------------------------
-// <copyright file="SelfReviveAbility.cs" company="Mistaken">
+// <copyright file="RevivedShield.cs" company="Mistaken">
 // Copyright (c) Mistaken. All rights reserved.
 // </copyright>
 // -----------------------------------------------------------------------
 
-using System.Linq;
-using CustomPlayerEffects;
-using Exiled.API.Features;
-using Exiled.CustomRoles.API.Features;
-using InventorySystem.Items.Usables;
-using Mistaken.API.Extensions;
-using Mistaken.API.GUI;
+using System.Collections;
+using System.Collections.Generic;
 using Mistaken.API.Shield;
 using UnityEngine;
 
 namespace Mistaken.TAU5
 {
-
     /// <inheritdoc/>
     public class RevivedShield : Shield
     {
         /// <inheritdoc/>
-        protected override int MaxShield => 1000;
+        protected override int MaxShield => this.maxShield;
 
         /// <inheritdoc/>
         protected override float ShieldRechargeRate => 500;
@@ -33,14 +27,37 @@ namespace Mistaken.TAU5
         protected override float TimeUntilShieldRecharge => 0f;
 
         /// <inheritdoc/>
+        protected override float ShieldDropRateOnOverflow => 200f;
+
+        /// <inheritdoc/>
         protected override void Start()
         {
             base.Start();
-            this.Invoke(nameof(this.Disable), 15);
+            this.StartCoroutine(this.Disable());
+
+            foreach (var item in this.gameObject.GetComponents<Shield>())
+            {
+                if (item != this && item.enabled)
+                {
+                    item.enabled = false;
+                    this.disabledShields.Add(item);
+                }
+            }
         }
 
-        private void Disable()
+        private readonly List<Shield> disabledShields = new List<Shield>();
+        private int maxShield = 1000;
+
+        private IEnumerator Disable()
         {
+            yield return new WaitForSeconds(15);
+            this.maxShield = 0;
+            while (this.Player.ArtificialHealth > 0)
+                yield return new WaitForSeconds(.1f);
+
+            foreach (var item in this.disabledShields)
+                item.enabled = true;
+            this.disabledShields.Clear();
             Destroy(this);
         }
     }
